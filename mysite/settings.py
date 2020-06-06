dafac8e4137c998dd3f543dfdb8a00c8758c7d21
sepulm01 <rguax910>
2020-06-05 21:43:24 -0400

    db fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -236,13 +236,14 @@ DATABASES = {
     'default': {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
-        'HOST': 'localhost',
-        'NAME': 'project.db',
+        #'HOST': 'localhost',
+        'NAME': os.path.join(BASE_DIR, 'project.db'),
         'PASSWORD': '',
         'PORT': '',
         'USER': ''
     }
 }
+
 
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
